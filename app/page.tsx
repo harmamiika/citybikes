33ed330f4e3390b01fd018956869c464294a1eb2
Miika Härmä <harmamiika@gmail.com>
2023-10-26 16:39:13 +0300

    stationlist initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,67 @@
 import Link from "next/link"
+import { Pool } from "pg"
 
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
-export default function IndexPage() {
+const pool = new Pool({
+  user: "academy",
+  host: "localhost",
+  database: "citybike",
+  password: "academy",
+  port: 5432,
+})
+
+interface IStation {
+  id: number
+  station_name: string
+  station_address: string
+  coordinate_x: string
+  coordinate_y: string
+}
+
+// todos:
+// single page view with link
+// cleanup header
+// add icons
+
+export default async function IndexPage() {
+  const result = await pool.query("SELECT * FROM station")
+  const stations = result.rows as IStation[]
+
+  console.log(stations[0])
+
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div>
+    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 flex-col">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Address</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {stations.map((station) => (
+            <TableRow>
+              <TableCell className="font-medium">
+                <Link href={`/station/${station.id}`}>
+                  {station.station_name}
+                </Link>
+              </TableCell>
+              <TableCell>{station.station_address}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </section>
   )
 }
